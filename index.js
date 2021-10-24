01119b7a0db271369ba8f01333d652f2d18736ea
Banee Ishaque K <baneeishaque@gmail.com>
2021-10-24 23:40:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    //   res.send('Hello World!')
+    const file = `${__dirname}/sample.png`;
+    res.download(file); // Set disposition and send it.̥
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
